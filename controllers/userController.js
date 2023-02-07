fea60f9b791be56c7fea3caedf2106aef73977b5
SeanBristol-Lee <seanbristol81@gmail.com>
2023-02-06 17:08:50 -0800

    add user controller

---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought } = require('../models');
+
+const totalUser = async () =>
+User.aggregate()
+    .count('userCount')
+    .then((numberOfUsers) => numberOfUsers); 
+
+module.exports = {
+    getUsers(req, res) {
+        User.find()
+        .then(async (users) => {
+            const userObj = {
+                users,
+                totalUser: await totalUser(),
+            };
+            return res.json(userObj);
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        });
+    },
+};
